5054a01a01b25f1ea35c4205357069bdd51ca422
davidS <david.sola.sole@gmail.com>
2015-10-15 18:00:18 +0200

    practicant

--- haskell/problems/P25054_ca.hs
+++ haskell/problems/P25054_ca.hs
@@ -4,7 +4,13 @@ myLength (x:xs) = 1 + myLength xs
 
 myMaximum::[Int] -> Int
 myMaximum [x] = x
-myMaximum (x:xs) = max x (myMaximum xs)
+myMaximum (x1: x2: xs)  
+	| x1 >= x2 = myMaximum (x1:xs)
+	| otherwise = myMaximum (x2:xs)
 
-average::[Int] -> Int
-average [x] = x
+-- average::[Int] -> Float
+-- average (x:xs) = 
+--	let sum = fromIntegral $ x + (average xs); len = fromIntegral $ (myLength xs) + 1
+--	in (fromIntegral sum) / (fromIntegral len)
+
+
